d8f67a8f81f9d2c10105679fd0740606fa7164d8
abdalla <abdallah.hesham@msa.edu.eg>
2024-03-30 18:54:56 -0700

    check

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -70,7 +70,7 @@ class FileStorage:
         self.reload()
 
     def get(self, cls, id):
-        """Returns the object based on the class and its ID, or None if not found"""
+        """Returns the object based on the class and its ID"""
         for clss in classes:
             if cls is classes[clss]:
                 cc = classes[clss]
