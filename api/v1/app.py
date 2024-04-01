ee42e2f98c2ab727c461ccf912999d66bdc86397
abdalla <abdallah.hesham@msa.edu.eg>
2024-03-31 21:35:52 -0700

    check modification

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
 """entry point for the flask api program"""
+
 from flask import Flask
+import os
 from .views import *
 from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
+    """exception"""
     storage.close()
 
-# @app_views.route('/status', strict_slashes=False)
-# def status():
-#     print('xxxxxxxxxxxxxxxxxxx')
-#     return 'hello world'
-#
-# @app_views.route('/', strict_slashes=False)
-# def index():
-#     return 'index'
 
 if __name__ == '__main__':
     """the entry point for the app"""
 
-    app.run(host='0.0.0.0', port='5000', debug=True, threaded=True)
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', '5000'))
 
+    app.run(host, port, debug=True, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """index if the views """
+
 from . import app_views
 from flask import jsonify
+
+
 @app_views.route('/status', methods=['GET'])
 def status():
     print('xxxxxxxxxxxxxxxxxxx')
     return jsonify({'status': "OK"})
+
 
 @app_views.route('/', strict_slashes=False)
 def index():
