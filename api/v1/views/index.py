13aa8036ae2d610a615a461068839c17906a076d
abdalla <abdallah.hesham@msa.edu.eg>
2024-04-01 14:23:48 -0700

    check task three

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,12 +3,25 @@
 
 from . import app_views
 from flask import jsonify
+from models.engine.db_storage import *
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
-    print('xxxxxxxxxxxxxxxxxxx')
     return jsonify({'status': "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    storage = DBStorage()  # Ensure DBStorage is instantiated
+    storage.reload()  # Make sure the session is loaded
+    count_dict = {}
+    for class_name in classes.keys():
+        print(classes.keys)
+        count_dict[class_name.lower()] = storage.count(class_name)
+
+    print(count_dict)  # Or return this in a response
+    return jsonify(count_dict)  # Convert the count_dict into a JSON response
 
 
 @app_views.route('/', strict_slashes=False)
