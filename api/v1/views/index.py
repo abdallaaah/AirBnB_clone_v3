41d594f7862ba3f60bfc72c95e171fc084ee1ee0
abdalla <abdallah.hesham@msa.edu.eg>
2024-04-01 18:34:47 -0700

    check pycodestyle handle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,15 +6,19 @@ from flask import jsonify
 from models.engine.db_storage import *
 from models import storage
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
+    """status will be here"""
     return jsonify({'status': "OK"})
+
 
 @app_views.route('/stats', methods=['GET'])
 def stats():
+    """stats will ber here"""
     count_dict = {}
-    for cls in classes:
-        count_dict[cls] = storage.count(classes[cls])
+    for cls_key in classes:
+        count_dict[cls_key] = storage.count(classes[cls_key])
     return jsonify(count_dict)
 
 
