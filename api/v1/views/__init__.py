dc28a916b8550687abfc2459531f7c4822f54710
abdalla <abdallah.hesham@msa.edu.eg>
2024-04-01 19:09:29 -0700

    check nw

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,8 +2,11 @@
 """the init file of views"""
 
 from flask import Blueprint
+# from api.v1.views.states import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')\
 
 
 from . import index
+from . import states
+
